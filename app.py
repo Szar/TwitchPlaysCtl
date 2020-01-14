a86f07fbeef54e522556df689bcc38b5428b3706
Szar <aly.hennessy@gmail.com>
2020-01-14 13:08:40 -0500

    New version of standalone bot

--- app.py
+++ app.py
@@ -1,55 +1,182 @@
-from flask_api import FlaskAPI, status, exceptions
-from flask_api.decorators import set_renderers
-from multiprocessing import Pool
-import os, sys, threading, irc.bot, random, time, json
-from twitchplaysbot import TwitchBotController
-import asynctwitch
+import os, sys, irc.bot, random, time, json, requests
+#from ctl import Ctl
+#import asynctwitch
 
 with open("config.json", "r") as f: cfg = json.load(f)
 
-print("Token Auth URL: ")
-print("https://id.twitch.tv/oauth2/authorize?client_id="+cfg["twitch"]["client_id"]+"&redirect_uri="+cfg["twitch"]["redirect_uri"]+"&response_type=token&scope=chat:edit%20chat:read%20user:read:email%20user:read:broadcast%20channel:read:subscriptions%20bits:read%20analytics:read:games")
+#print("Token Auth URL: ")
+#print("https://id.twitch.tv/oauth2/authorize?client_id="+cfg["twitch"]["client_id"]+"&redirect_uri="+cfg["twitch"]["redirect_uri"]+"&response_type=token&scope=chat:edit%20chat:read%20user:read:email%20user:read:broadcast%20channel:read:subscriptions%20bits:read%20analytics:read:games")
 
+class TwitchController():
+	def __init__(self):
+		#self.ctl = Ctl(self.update, self.stopped, self.title)
+		self.prompt = False
 
-app = FlaskAPI(__name__)
-twitchplaysbot = TwitchBotController()
-
-bot = asynctwitch.CommandBot(
-			user = cfg["twitch"]["bot_username"],
-			oauth = 'oauth:'+cfg["twitch"]["bot_token"],  # oauth:1234567890abcdefghijklmnopqrst
-			channel = cfg["twitch"]["channel"],     
-			prefix = "!",  
-		)
-@bot.command('example', alias=['moreexample','anothaone'], desc='example command')
-async def example(message, word1:str, number1:int, rest:str):
-	bot.say(message.channel.name, 'wow')
-
-
-@bot.command('new-prompt', alias=[], desc='Restart prompt')
-async def newprompt(message):
-	twitchplaysbot.newprompt()
-	#bot.say('restarting...')
-
-@bot.command('prompt', alias=[], desc='Set new prompt')
-async def prompt(message, subcommand:str):
-	twitchplaysbot.prompt(subcommand)
-
-thr = threading.Thread(target=bot.start, args=(), kwargs={})
-thr.start()
-
-if (sys.version_info > (3, 0)): app.config['DEFAULT_RENDERERS'] = ['flask_api.renderers.JSONRenderer']
-else: app.config['DEFAULT_RENDERERS'] = [ 'flask.ext.api.renderers.JSONRenderer', ]
-
-@app.route("/", methods=['GET', 'POST'])
-def main():
-	return {
-			"data":twitchplaysbot.get("data"),
-			"scores":twitchplaysbot.get("scores"),
-			"prompt":twitchplaysbot.get("prompt")
-		}
-
-if __name__ == "__main__":
-	app.run(host= '0.0.0.0', port=cfg["defaults"]["port"], debug=False)
+	def addPrompt(self,message):
+		prompt = message["command_text"]
+		r = requests.post(cfg["defaults"]["api_url"]+"?action=add_prompt", data=message).json()
+		print("=== addPrompt ===")
+		print(json.dumps(r))
+		print("==================")
+		return r[0]
 	
-	#thr = threading.Thread(target=bot.start, args=(), kwargs={})
+	def getUser(self,message):
+		r = requests.post(cfg["defaults"]["api_url"]+"?action=user", data=message).json()
+		print("=== getUser ===")
+		print(json.dumps(r))
+		print("==================")
+		return r
+
+	def getCurrentPrompt(self):
+		r = requests.get(cfg["defaults"]["api_url"]+"?action=get_prompt").json()
+		print("=== getCurrentPrompt ===")
+		print(json.dumps(r))
+		print("==================")
+		return r
+
+	def startPrompt(self, message):
+		self.prompt = self.addPrompt(message)
+		self.ctl.start(self.prompt["prompt"])
+
+	def stopPrompt(self):
+		self.ctl.stop()
+		
+	def update(self, txt):
+		self.prompt["text"] = txt
+		r = requests.post(cfg["defaults"]["api_url"]+"?action=update_prompt", data={"text":txt, "id":self.prompt["id"]}).json()
+		print("=== update ===")
+		print(json.dumps(r))
+		print("==================")
+		#return r
+
+	def stopped(self):
+		r = requests.get(cfg["defaults"]["api_url"]+"?action=deactivate").json()
+		print("=== stopPrompt ===")
+		print(json.dumps(r))
+		print("==================")
+		#return r
+
+	def title(self, txt):
+		print("=== title ===")
+		print(self.prompt["prompt"])
+		print(txt)
+		print("==================")
+
+
+
+	"""
+
+	def title(self, txt):
+		self.setv("prompt",txt)
+		#self.botData["prompt"] = txt
+
+	def stopped(self):
+		self.botData["data"] = ""
+		self.botData["prompt"] = "Waiting for new prompt..."
+
+	def prompt(self,txt):
+		print(txt)
+		#self.setv("prompt",txt)
+		#
 	
+	def newprompt(self):
+		pass
+		#
+	
+	def setv(self,k,v):
+		self.botData[k] = v
+	
+	def get(self, k):
+		return self.botData[k]"""
+
+
+
+
+
+
+class TwitchBot(irc.bot.SingleServerIRCBot):
+	def __init__(self, username, client_id, token, channel):
+		self.client_id = client_id
+		self.token = token
+		self.channel = '#' + channel
+		self.controller = TwitchController()
+		
+		r = requests.get('https://api.twitch.tv/kraken/users?login=' + channel, headers={'Client-ID': client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}).json()
+		self.channel_id = r['users'][0]['_id']
+		
+		irc.bot.SingleServerIRCBot.__init__(self, [('irc.chat.twitch.tv', 6667, 'oauth:'+token)], username, username)
+		print("[ Connected to Twitch ]") 
+		
+
+
+	def on_welcome(self, c, e):
+		c.cap('REQ', ':twitch.tv/membership')
+		c.cap('REQ', ':twitch.tv/tags')
+		c.cap('REQ', ':twitch.tv/commands')
+		c.join(self.channel)
+		print("[ Bot Running ]") 
+		#self.controller.addPrompt(sample_message)
+		self.controller.stopPrompt()
+		
+	def on_pubmsg(self, c, e):
+		if e.arguments[0][:1] == '!':
+			cmd = e.arguments[0].split(' ')[0][1:]
+			print('[ Received command: ' + cmd+ ' ]')
+			self.do_command(e, cmd)
+		return
+
+	def set_message(self,e,cmd):
+		tag_keys = ["display-name","id","mod","subscriber","turbo","user-id","user-type"] #badges, flags
+		message = { "channel": e.target, "text": e.arguments[0].strip(), "command": cmd, "command_text": e.arguments[0].strip().replace("!"+cmd+" ",""), "username": e.source.split("!")[0] }
+		for t in e.tags:
+			if t["key"] in tag_keys:
+				message[t["key"]] = t["value"]
+		return message
+
+
+	def do_command(self, e, cmd):
+		c = self.connection
+		message = self.set_message(e, cmd)
+		user = self.controller.getUser(message)
+		
+		if cmd == "prompt":
+			self.controller.startPrompt(message)
+			time.sleep(10)
+			self.controller.update("how far can you do this thing addon text")
+		
+		elif cmd == "new-prompt":
+			self.controller.stopPrompt()
+
+
+		
+		
+		c.privmsg(self.channel, "[Received Command] \""+message["text"]+"\"")
+
+		 # channel
+		
+		
+		#
+		"""if cmd == "new-prompt":
+			c.privmsg(self.channel, str(is_running))
+			if is_running:
+				c.privmsg(self.channel, "[IncelBot] Restarting... please wait.")
+				stopgen()
+				
+				
+			
+		elif cmd == "prompt":
+			if is_running is False:
+				txt = e.arguments[0].replace("!prompt ","").strip()
+				if txt!="":
+					c.privmsg(self.channel, "[IncelBot] Using prompt \""+txt+"\"")
+					rungen(txt)
+					#rungen(txt)
+		elif cmd == "seed":
+			setSeed(int(e.arguments[0].replace("!seed ","").strip()))"""
+
+bot = TwitchBot(cfg["twitch"]["bot_username"], cfg["twitch"]["client_id"], cfg["twitch"]["bot_token"], cfg["twitch"]["channel"])
+bot.start()
+
+#twitchplaysbot = TwitchController()
+#twitchplaysbot.newprompt()
+#twitchplaysbot.prompt(subcommand)
